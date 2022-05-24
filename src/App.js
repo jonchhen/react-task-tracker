27849062a313f277a081c42c43bda52d8b6dd0d8
Ashish Jonchhen <jonchhen.ashish@hotmail.com>
2022-05-24 16:45:55 -0500

    added addTask, check and uncheck reminder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
 
 import React from "react"
 
@@ -14,22 +15,44 @@ const App = () => {
     },
     {
       id: 2,
-      text: "Meeting Appointment",
+      text: "Meeting With Hair Sylist",
       day: "Feb 6th at 6:10pm",
       reminder: false,
     },
+    {
+      id: 3,
+      text: "Movie Date",
+      day: "Feb 8th at 7:00pm",
+      reminder: true,
+    },
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000)
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show !!!"
       )}
