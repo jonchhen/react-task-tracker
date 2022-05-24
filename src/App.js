dc3fbebdaec72fee4b44c6a621a60c8418192f76
Ashish Jonchhen <jonchhen.ashish@hotmail.com>
2022-05-24 17:25:31 -0500

    Add the functionality to the add/close button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddTask from "./components/AddTask"
 import React from "react"
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -49,8 +51,11 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
